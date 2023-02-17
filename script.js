6b846601060bc35498913a717b5b321f00a423f8
HackerAnurag <88178410+HackerAnurag@users.noreply.github.com>
2023-02-17 20:21:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// This is an example of a JavaScript file, which can be used to add interactivity to the website
+// For example, you can use JavaScript to validate the form input, display a message when the form is submitted, or add animations to the website
